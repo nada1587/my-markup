b2a4e5bda3bc6d88b9e973a8864ab9983f8469fb
nada1587 <nada1587@naver.com>
2020-01-31 18:29:31 +0900

    drag N drop  확인

--- src/views/markup/common/MainHeader.vue
+++ src/views/markup/common/MainHeader.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <header class="main-header">
-      <button type="button" class="btn-back">뒤로</button>
-      <h1>위메프투어</h1>
+  <header class="main-header">
+    <button type="button" class="btn-back">뒤로</button>
+    <h1>위메프투어의 제목은 어디까지 길어질수 있을까?</h1>
+    <div>
       <button type="button" class="btn-close">닫기</button>
-    </header>
-  </div>
+      <button type="button" class="btn-close">닫기</button>
+    </div>
+  </header>
 </template>
 
 <script>
--- src/views/markup/main/Main.vue
+++ src/views/markup/main/Main.vue
@@ -2,26 +2,66 @@
   <div>
     <main-header></main-header>
     <div>
-      <ul class="u_list_horizontal recommend">
+      <ul class="u-list-horizontal recommend">
         <li v-for="list in 7" :key="list">
           <a href="#" style="display:inline-block;width:100px;">{{ list }}</a>
         </li>
       </ul>
+    </div>
+
+    <div>
+      <ul class="flex-box">
+        <li><a href="#">#가</a></li>
+        <li><a href="#">#가나</a></li>
+        <li><a href="#">#가나다</a></li>
+        <li><a href="#">#가나다라</a></li>
+        <li><a href="#">#가나다라마</a></li>
+        <li><a href="#">#가나다라마바</a></li>
+        <li><a href="#">#가나다라마바사</a></li>
+        <li><a href="#">#가나다라마바사아</a></li>
+        <li><a href="#">#가나다라마바사아자</a></li>
+        <li><a href="#">#가나다라마바사아자차</a></li>
+      </ul>
+    </div>
+
+    <div class="u-drag-box">
+      <Container @drop="onDrop" drag-handle-selector=".column-drag-handle">
+        <Draggable v-for="item in items" :key="item.id">
+          <div class="draggable-item">
+            <i>비행기</i>
+            <div>
+              <span class="column-drag-handle">&#x2630;</span>
+              <div class="item-inner">
+                {{item.data}}
+              </div>
+            </div>
+          </div>
+        </Draggable>
+      </Container>
     </div>
   </div>
 </template>
 
 <script>
 import MainHeader from '@/views/markup/common/MainHeader.vue'
+import { Container, Draggable } from "vue-smooth-dnd";
+import { applyDrag, generateItems } from "@/utils/helpers";
 
 export default {
   name: 'Main',
   components: {
+    Container,
+    Draggable,
     MainHeader,
   },
   data() {
     return {
-
+      items: generateItems(15, i => ({ id: i, data: "Draggable " + i }))
+    };
+  },
+  methods: {
+    onDrop(dropResult) {
+      this.items = applyDrag(this.items, dropResult);
     }
   }
 }
